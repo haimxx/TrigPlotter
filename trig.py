51e08fa62ae49dd469b6bdad2b4c37735a4b7fec
haim <haimxx@hotmail.com>
2020-01-07 21:51:58 +0200

    file exeception handling

--- trig.py
+++ trig.py
@@ -1,26 +1,60 @@
 #!/usr/bin/env python3
 
-#author: Haim
+# author: Haim
 
 import matplotlib.pyplot as plt
 import numpy as np
 
-# setting range
-start = 0
-end = 4*np.pi
-step = 0.1
+start, end, step = 0, 0, -1
+file_name = "range"
 
-x = np.arange(start, end, step)
+# default values
+default_start = 0
+default_end = 4
+default_step = 0.1
+
+
+def check_values(start, end, step):
+    """
+    Function to check whether start, end and step values are valid
+    """
+
+    if start == end:
+        print("Start and end values must be different! Using default values")
+        start, end = default_start, default_end
+
+    if start > end:
+        print("Start value must be greater than end value! Using default values")
+        start, end = default_start, default_end
+
+    if step <= 0:
+        print("Non-positive value for step argument! Using default value")
+        step = default_step
+
+
+# setting range
+try:
+    with open(file_name) as fo:
+        info = fo.read()
+    start, end, step = [float(i) for i in info.split()]
+except FileNotFoundError:
+    print("File wasn't found! Using default values")
+    start, end, step = default_start, default_end, default_step
+
+# sanity check
+check_values(start, end, step)
+
+x = np.arange(start*np.pi, end*np.pi, step)
 y = np.sin(x)
 z = np.cos(x)
 
-xlabel = 'x values from ' + str(start) + ' to ' + str(end)
-title = 'Plot of sin and cos from ' + str(start) + ' to ' + str(end)
+xlabel = "x"
+title = "Plot of sine and cosine from " + str(start) + "π to " + str(end) + "π"
 
 plt.xlabel(xlabel)
-plt.ylabel('sin(x) and cos(x)')
+plt.ylabel("sin(x) and cos(x)")
 plt.title(title)
-plt.legend(['sin(x)', 'cos(x)'])
+plt.legend(["sin(x)", "cos(x)"])
 
 plt.plot(x, y, x, z)
 plt.show()
